97027f45e6e674b025e5fed0b4ac5b9322c8418e
elreys <gdilisa@hotmail.com>
2019-11-04 12:24:46 -0800

    Fixed Gesture Package

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+
+
+const ListScreen = () => {
+
+    const friends = [
+        { name: "Friends #1" },
+        { name: "Friends #2" },
+        { name: "Friends #3" },
+        { name: "Friends #4" },
+        { name: "Friends #5" },
+        { name: "Friends #6" },
+        { name: "Friends #7" },
+        { name: "Friends #8" },
+        { name: "Friends #9" },
+        { name: "Friends #10" },
+        { name: "Friends #11" },
+
+    ]
+
+    return (
+        <FlatList
+            data={friends}
+            renderItem={({item}) => {
+                return <Text>{item.name}</Text>
+            }}
+
+        >
+        </FlatList>
+    );
+}
+
+const styles = StyleSheet.create({
+
+
+})
+
+export default ListScreen;
