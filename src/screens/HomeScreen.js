3983b08e25d7191dd86a63eb4b81999183e90111
MMBBS0000 <giuseppedilisa@CHN0004.local>
2019-11-05 11:56:22 +0100

    added Button and Navigation Feature

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button,TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>test</Text>;
+const HomeScreen = ({navigation}) => {
+  
+  return (
+    <View>
+      <Text style={styles.text}>Hi there !</Text>
+      <Button 
+      title="Go to ComponentsDemo" 
+      onPress={() => navigation.navigate("Components")
+      }>
+
+      </Button>
+      <Button title="Go to List Demo"
+      onPress={() => navigation.navigate("List")}></Button>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
